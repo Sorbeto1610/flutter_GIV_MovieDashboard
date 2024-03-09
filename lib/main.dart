0b79298f3b0bd578364a1e71abea3b662c60602b
IreneDagher <irene_dagher@hotmail.com>
2024-03-09 12:41:53 +0100

    Ajout de Carrousel et modification de la position du grid layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,99 @@
-
-import 'package:flutter/material.dart';//un truc qu'on installe par defaut
-//le package pour pouvoir avoir a grid
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(
-      MaterialApp(
-          debugShowCheckedModeBanner: false,//pour enlever le truc en mode debug
-          home:BasicGridWidget()//trouve le code dans BASIC GRID WIDGET
-      ),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BasicGridWidget(),
+    ),
   );
 }
-
 
 class BasicGridWidget extends StatelessWidget {
   const BasicGridWidget();
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text("GIV SUPERBOWL WEBSITE"),
-      centerTitle:true,
-
-    ),
-
-
-      body:Stack(
-          children:<Widget>[
-            Align(
-            alignment:Alignment.topCenter,
-              child: Container(
-                decoration:const BoxDecoration(
-
-                gradient:LinearGradient(
-                begin: Alignment.center,
-                 end:Alignment.bottomCenter,
-                    colors:[
-                        Colors.purple,
-                        Colors.red,
-
-                     ]
-           ),
-
-           ),
-
-            height: MediaQuery.of(context).size.height* 0.25, // Set the height to 1/4 of the screen height
-
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GIV SUPERBOWL WEBSITE"),
+        centerTitle: true,
       ),
-Positioned(
-  top: 50,
-  left:MediaQuery.of(context).size.width* 0.25,
-  width:  MediaQuery.of(context).size.width* 0.5,
-  height: MediaQuery.of(context).size.height* 0.5,
-
-            child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          mainAxisSpacing: 7, //le spacing entre deux lignes
-          crossAxisSpacing: 10, //le spacing entre colonne
-          crossAxisCount: 4, //le nombre de colonne
-          ),
-           itemCount: 50, //nombre de item que je veux dans ma grid view
-             itemBuilder: (context, index) =>  buildImageCard(index), //ici c'est pour fill les grid avec ces elements
-                                 ),
-
-          ),
-
-
-
-
-
-          ],
-      )
-  );
-
-  }
-
-  Widget buildImageCard(int index) =>
-      Card(
-        margin: EdgeInsets.zero,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child:Directionality(
-        textDirection: TextDirection.ltr,
-          child: Container(
-          margin: EdgeInsets.all(8),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.network(
-              'https://source.unsplash.com/random?sig=$index',
-              fit: BoxFit.cover,
+      body: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.center,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.purple, Colors.red],
+                ),
+              ),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                  crossAxisCount: 5,
+                ),
+                itemCount: 50,
+                itemBuilder: (context, index) => buildImageCard(index),
+              ),
+              height: MediaQuery.of(context).size.height * 0.25,
             ),
           ),
+
+          Positioned(
+            height: MediaQuery.of(context).size.height/2,
+            width: MediaQuery.of(context).size.width/3,
+            top:  MediaQuery.of(context).size.height/4,
+            left: 20,
+            child: CarouselSlider(
+              options: CarouselOptions(
+                height: MediaQuery.of(context).size.height * 0.25,
+              ),
+              items: [1, 2, 3, 4, 5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width/3,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                      ),
+                      child: Text('text $i', style: TextStyle(fontSize: 16.0)),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildImageCard(int index) => Card(
+    margin: EdgeInsets.zero,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(8),
+    ),
+    child: Directionality(
+      textDirection: TextDirection.ltr,
+      child: Container(
+        margin: EdgeInsets.all(8),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Image.network(
+            'https://source.unsplash.com/random?sig=$index',
+            fit: BoxFit.cover,
+          ),
         ),
-        ),
-      );
-
-
-
+      ),
+    ),
+  );
 }
