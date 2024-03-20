eff7f74c96903e110425ff9a7a439733f6afcb40
IreneDagher <irene_dagher@hotmail.com>
2024-03-20 15:29:29 +0100

    Afficher l'api !

---
+++ lib/api-config.dart
@@ -0,0 +1,5 @@
+class ApiConfig {
+  static const String apiKey = '457e86219abc8f213a939e7809567854';
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+}
