5a636dac9431d19e05baf67ed493cb98cac214bc
IreneDagher <irene_dagher@hotmail.com>
2024-03-25 18:04:24 +0100

    Implementation de garphe en bar avec selection du genre

--- lib/graphPage.dart
+++ lib/graphPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'movie.dart';
+import 'genre.dart';
 import 'fetchService.dart';
 import 'comparePopularity.dart'; // Importer la fonction comparePopularityList
 
@@ -11,11 +12,16 @@ class graphPage extends StatefulWidget {
 
 class _graphPageState extends State<graphPage> {
   late Future<List<Movie>> _moviesFuture;
+  late Future<List<Genre>> _genresFuture;
+  late List<Genre> _genresList;
+  Genre? _selectedGenre; // Genre sélectionné
 
   @override
   void initState() {
     super.initState();
     _moviesFuture = FetchService.fetchMoviesTrend();
+    _genresFuture = FetchService.fetchMoviesListGenre();
+    _selectedGenre = null; // Initialiser le genre sélectionné à null
   }
 
   @override
@@ -25,19 +31,16 @@ class _graphPageState extends State<graphPage> {
         title: Text('Movie Popularity Chart'),
       ),
       body: Center(
-        child: FutureBuilder<List<Movie>>(
-          future: _moviesFuture,
+        child: FutureBuilder<List<Genre>>(
+          future: _genresFuture,
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return CircularProgressIndicator();
             } else if (snapshot.hasError) {
               return Text('Error: ${snapshot.error}');
             } else {
-              return Container(
-                height: 400,
-                padding: EdgeInsets.all(20.0),
-                child: _buildBarChart(snapshot.data!),
-              );
+              _genresList = snapshot.data!;
+              return _buildBody();
             }
           },
         ),
@@ -45,13 +48,84 @@ class _graphPageState extends State<graphPage> {
     );
   }
 
-  Widget _buildBarChart(List<Movie> movies) {
-    // Tri de la liste de films par popularité (du moins populaire au plus populaire)
-    movies.sort((a, b) => a.popularity.compareTo(b.popularity));
+  Widget _buildBody() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _buildGenreDropdown(),
+        SizedBox(height: 20),
+        Expanded(
+          child: FutureBuilder<List<Movie>>(
+            future: _moviesFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return _buildGraph(snapshot.data!);
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
 
-    // Appel de comparePopularityList avec la liste triée de films
+  Widget _buildGenreDropdown() {
+    return DropdownButton<Genre>(
+      value: _selectedGenre,
+      onChanged: (Genre? newValue) {
+        setState(() {
+          _selectedGenre = newValue;
+        });
+      },
+      items: [
+        DropdownMenuItem<Genre>(
+          value: null,
+          child: Text('All'),
+        ),
+        ..._genresList.map<DropdownMenuItem<Genre>>((Genre genre) {
+          return DropdownMenuItem<Genre>(
+            value: genre,
+            child: Text(genre.name),
+          );
+        }).toList(),
+      ],
+      style: TextStyle(fontSize: 16.0), // Style pour le texte du bouton déroulant
+      dropdownColor: Colors.grey[200], // Couleur du menu déroulant
+      elevation: 5, // Élévation du menu déroulant
+      icon: Icon(Icons.arrow_drop_down), // Icône du bouton déroulant
+      isExpanded: true, // Définir la largeur du bouton déroulant sur la largeur de son parent
+      underline: Container(), // Retirer la ligne en dessous du bouton déroulant
+    );
+  }
+
+  Widget _buildGraph(List<Movie> movies) {
+    // Filtrer les films par genre sélectionné
+    final List<Movie> filteredMovies = _selectedGenre != null
+        ? movies.where((movie) => movie.genreIds.contains(_selectedGenre!.id)).toList()
+        : List.from(movies); // Copie la liste complète de films si aucun genre n'est sélectionné
+
+    // Si la liste de films filtrée est vide, afficher un message avec un GIF
+    if (filteredMovies.isEmpty) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/loading.gif'), // Remplacer 'assets/loading.gif' par le chemin de votre GIF
+          SizedBox(height: 20),
+          Text('Aucun film à afficher'),
+        ],
+      );
+    }
+
+    // Tri de la liste de films filtrée par popularité (du moins populaire au plus populaire)
+    filteredMovies.sort((a, b) => a.popularity.compareTo(b.popularity));
+
+    // Appel de comparePopularityList avec la liste de films triée par popularité
     final List<charts.Series<Movie, String>> seriesList =
-    comparePopularityList(movies);
+    comparePopularityList(filteredMovies.reversed.toList()); // Inverser la liste pour afficher du moins populaire au plus populaire
 
     return charts.BarChart(
       seriesList,
@@ -60,4 +134,3 @@ class _graphPageState extends State<graphPage> {
     );
   }
 }
-
