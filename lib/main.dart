9e7a744673bc6a46557c3ee725e687969454deab
IreneDagher <irene_dagher@hotmail.com>
2024-03-25 18:04:24 +0100

    Implementation de garphe en bar avec selection du genre

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,21 @@ import 'trendingMovies.dart';
 import 'image_carousel_slider.dart';
 import 'piechartPage.dart';
 import 'graphPage.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
-
-
-
 
 
 void main() {
-   runApp(
-     MaterialApp(
-       title: "Project GIV",
-       debugShowCheckedModeBanner: false,
-       home: BasicGridWidget(),
-     ),
-   );
- }
-
-
+  runApp(
+    MaterialApp(
+      title: "Project GIV",
+      debugShowCheckedModeBanner: false,
+      home: BasicGridWidget(),
+    ),
+  );
+}
 
 class BasicGridWidget extends StatelessWidget {
-  const BasicGridWidget({super.key});
+  const BasicGridWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,70 +25,64 @@ class BasicGridWidget extends StatelessWidget {
 
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
-        child:Column(
-        children: <Widget>[
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color(0xFF91180B),
-                  Color(0xFFB9220F),
-                  Color(0xFFE32D13),
-                  Color(0xFFF85138),
-                  Color(0xFFFF6666),
-                ],
+        child: Column(
+          children: <Widget>[
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFF91180B),
+                    Color(0xFFB9220F),
+                    Color(0xFFE32D13),
+                    Color(0xFFF85138),
+                    Color(0xFFFF6666),
+                  ],
+                ),
+              ),
+              height: MediaQuery.of(context).size.height *
+                  MediaQuery.of(context).size.width /
+                  6000, // Adjust accordingly
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 5,
+                  mainAxisSpacing: 3.0,
+                  crossAxisSpacing: 8.0,
+                ),
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return ImageCarouselSlider();
+                },
               ),
             ),
-            height: MediaQuery.of(context).size.height * MediaQuery.of(context).size.width / 6000, // Adjust accordingly
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 5,
-                mainAxisSpacing: 3.0,
-                crossAxisSpacing: 8.0,
-              ),
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return ImageCarouselSlider();
-              },
+            SizedBox(height: 20), // Add space between GridView and title
+            ResponsiveText(
+              text: "The Movie Database",
+              fontSize: 24.0,
+              textColor: Colors.white,
+              shadowColor: Colors.grey,
+              shadowOffset: Offset(2.0, 2.0),
             ),
-          ),
-          SizedBox(height: 20), // Add space between GridView and title
-          ResponsiveText(
-            text: "The Movie Database",
-            fontSize: 24.0,
-            textColor: Colors.white,
-            shadowColor: Colors.grey,
-            shadowOffset: Offset(2.0, 2.0),
-          ),
-          SizedBox(height: 20), // Add space between title and trending movies
-          Container(
-            height: MediaQuery.of(context).size.height / 1.55,
-            child: TrendingMoviesHomePage(),
-          ),
-          SizedBox(height: 20), // Add space between trending movies and dashboard
-          Container(
-            height: MediaQuery.of(context).size.height / 1.9,
-            child: piechartPage(),
-          ),
-          SizedBox(height: 20), //
-          Container(
-            height: MediaQuery.of(context).size.height / 1.9,
-            child: graphPage(),
-          ),
-        ],
-      ),
+            SizedBox(height: 20), // Add space between title and trending movies
+            Container(
+              height: MediaQuery.of(context).size.height / 1.55,
+              child: TrendingMoviesHomePage(),
+            ),
+            SizedBox(height: 20), // Add space between trending movies and pie chart
+            Container(
+              height: MediaQuery.of(context).size.height / 1.9,
+              child: piechartPage(),
+            ),
+            SizedBox(height: 20), // Add space between pie chart and graph
+              Container(
+                height: MediaQuery.of(context).size.height ,
+                child: graphPage(),
+              ),
+          ],
+        ),
       ),
     );
-
   }
 }
-
-
-
-
-
-
-
 
 class ResponsiveText extends StatelessWidget {
   final String text;
@@ -102,14 +91,12 @@ class ResponsiveText extends StatelessWidget {
   final Color shadowColor;
   final Offset shadowOffset;
 
-
   ResponsiveText({
     required this.text,
     required this.fontSize,
     required this.textColor,
     required this.shadowColor,
     required this.shadowOffset,
-
   });
 
   @override
@@ -124,12 +111,10 @@ class ResponsiveText extends StatelessWidget {
         color: textColor,
         fontFamily: 'Montserrat',
         fontWeight: FontWeight.bold,
-
         shadows: [
           Shadow(
             color: shadowColor,
             offset: shadowOffset,
-
           ),
         ],
       ),
