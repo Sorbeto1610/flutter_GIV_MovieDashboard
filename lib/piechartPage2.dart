b252bb7543189fe2352bf370ffb3a0cd1694c221
valentine <vali.gobert@gmail.com>
2024-04-08 11:17:51 +0200

    boutons cliquables pour les piecharts

--- lib/piechartPage2.dart
+++ lib/piechartPage2.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'movie.dart';
 import 'fetchService.dart';
 import 'genre.dart';
-import 'movie.dart';
-import 'package:fl_chart/fl_chart.dart';
 
 class PiechartPage2 extends StatefulWidget {
+  final Function(Map<String, double>) onGenreSelected;
+
+  const PiechartPage2({Key? key, required this.onGenreSelected}) : super(key: key);
+
   @override
   _PiechartPage2State createState() => _PiechartPage2State();
 }
@@ -25,35 +29,18 @@ class _PiechartPage2State extends State<PiechartPage2> {
   Future<void> _loadData() async {
     final List<Movie> movies = await _moviesFuture;
     final List<Genre> genres = await _genresFuture;
-
-    // Calculate total popularity of all movies
     double totalPopularity = movies.fold(0, (acc, movie) => acc + movie.popularity);
-
     Map<String, double> genrePopularityMap = {};
-    List<Color> genreColors = [];
-
-    // Calculate popularity of each genre
     for (var genre in genres) {
       double genrePopularity = 0;
-
       for (var movie in movies) {
         if (movie.genreIds.contains(genre.id)) {
           genrePopularity += movie.popularity;
         }
       }
-
-      // Calculate percentage of popularity for the genre
       double genrePercentage = (genrePopularity / totalPopularity) * 100;
-
-      // Ensure genre percentage doesn't exceed 100%
-      if (genrePercentage > 100) {
-        genrePercentage = 100;
-      }
-
-      // Exclude genres with 0 percentage
       if (genrePercentage > 0) {
         genrePopularityMap[genre.name] = genrePercentage;
-        genreColors.add(getGenreColor(genre.name));
       }
     }
 
@@ -68,131 +55,44 @@ class _PiechartPage2State extends State<PiechartPage2> {
     setState(() {
       _genrePopularityMap = genrePopularityMap;
     });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      widget.onGenreSelected(_genrePopularityMap);
+    });
   }
-
-
 
   Color getGenreColor(String genreName) {
     return Colors.primaries[genreName.length % Colors.primaries.length];
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: Text(
-            'Popularity of genre',
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
+    return _genrePopularityMap.isNotEmpty
+        ? Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[900],
+          borderRadius: BorderRadius.circular(20.0),
         ),
-        body: FutureBuilder(
-          future: Future.wait([_moviesFuture, _genresFuture]),
-          builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: Image.asset('assets/clap.gif'));
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else {
-              return Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Center(
-                  child: Container(
-                    constraints: BoxConstraints.expand(),
-                    decoration: BoxDecoration(
-                      color: Colors.grey[900],
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: LayoutBuilder(
-                      builder: (context, constraints) {
-                        return Column(
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: AspectRatio(
-                                  aspectRatio: 1.0,
-                                  child: PieChart(
-                                    PieChartData(
-                                      sections: _genrePopularityMap.entries.map((entry) {
-                                        return PieChartSectionData(
-                                          value: entry.value,
-                                          title: '${entry.key}',
-                                          color: Colors.primaries[_genrePopularityMap.keys.toList().indexOf(entry.key)],
-                                          showTitle: true,
-                                          titlePositionPercentageOffset: 1.4,
-                                          radius: constraints.maxWidth > constraints.maxHeight
-                                              ? constraints.maxHeight / 8.5 // Adjust based on maxHeight for landscape mode
-                                              : constraints.maxWidth / 8.5, // Adjust based on maxWidth for portrait mode
-                                          titleStyle: TextStyle(
-                                            fontSize: constraints.maxWidth > constraints.maxHeight
-                                                ? constraints.maxHeight * 0.04 // Adjust font size based on maxHeight for landscape mode
-                                                : constraints.maxWidth * 0.04, // Adjust font size based on maxWidth for portrait mode
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.white,
-                                          ),
-                                        );
-                                      }).toList(),
-                                      borderData: FlBorderData(
-                                        show: true,
-                                        border: Border.all(
-                                          color: Colors.white,
-                                          width: 7,
-                                        ),
-                                      ),
-                                      sectionsSpace: 7,
-                                      centerSpaceRadius: constraints.maxWidth > constraints.maxHeight
-                                          ? constraints.maxHeight / 5 // Adjust based on maxHeight for landscape mode
-                                          : constraints.maxWidth / 5, // Adjust based on maxWidth for portrait mode
-                                      startDegreeOffset: -90,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 20,),
-                            Wrap(
-                              alignment: WrapAlignment.center,
-                              children: _genrePopularityMap.entries.map((entry) {
-                                final title = ' ${entry.key} (${entry.value.toStringAsFixed(1)}%) ';
-                                final color = Colors.primaries[_genrePopularityMap.keys.toList().indexOf(entry.key)];
-
-                                return Container(
-                                  margin: EdgeInsets.only(bottom: 5),
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Container(
-                                        width: 10,
-                                        height: 10,
-                                        color: color,
-                                      ),
-                                      SizedBox(width: 5),
-                                      Text(title,
-                                          style: TextStyle(color: Colors.white)),
-                                    ],
-                                  ),
-                                );
-                              }).toList(),
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                  ),
-                ),
+        child: PieChart(
+          PieChartData(
+            sections: _genrePopularityMap.entries.map((entry) {
+              return PieChartSectionData(
+                color: getGenreColor(entry.key),
+                value: entry.value,
+                title: entry.key, // Modification ici
+                titleStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
+                radius: 100,
               );
-            }
-          },
+            }).toList(),
+            borderData: FlBorderData(show: false),
+            sectionsSpace: 2,
+            centerSpaceRadius: 80,
+          ),
         ),
       ),
-    );
+    )
+        : Center(child: CircularProgressIndicator());
   }
 }
